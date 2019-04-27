5cc8b5fd78a1b7293d4cc1dba567edf1ccf582e1
BeatsInBytes <kaivernooy@gmail.com>
2019-04-26 20:57:19 -0400

    Update to page, added head

--- analysis.php
+++ analysis.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Beats in Bytes | Analysis Tools</title>
+
+        <link rel="stylesheet" type="text/css" href="style-main.css" />
+        <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic" rel="stylesheet">
+        <link rel="shortcut icon" type="image/png" href="image-assets/favicon.png"/>
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+        <script src="https://verovio-script.humdrum.org/scripts/verovio-toolkit.js"></script>
+        <script src="https://plugin.humdrum.org/scripts/humdrum-notation-plugin.js"></script>
+        <script> var vrvToolkit = new verovio.toolkit(); </script>
+
+    </head>
+    <body>
+
 <?php
     $filesrecieved = $_POST['userfilelocations'];
     $explodedfiledirs = explode(",", $filesrecieved);
@@ -16,11 +34,10 @@
             $analysisname = substr($selected, 0, strpos($selected, "."));
             $directory = $value . "/data/" . "$analysisname";
             mkdir($directory);
+            shell_exec("$scriptdirec $song");
 
             $scriptdirec = "analysis-scripts/bib-scripts/original/" . $selected;
 
-            $output = shell_exec("$scriptdirec $song");
-            echo $output;
             echo "<br>";
 
             // selected is the script, song is the file
@@ -34,5 +51,14 @@
         echo "<br>";
 
     }
+    foreach ($explodedfiledirs as $value) {
+        $datafolder = $value . "/data";
+        $keySigPath = $datafolder . "/key-signature/key-signature.txt";
+        $keysig = file_get_contents($keySigPath);
+        echo '<div class="panels"><p>' . $value . '</p><br><span>' . $keysig . '</span></div>';
+    }
+
 
 ?>
+</body>
+</html>
