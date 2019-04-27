3d3f4b38e5a323541d721aa814e86cf968b9f212
BeatsInBytes <kaivernooy@gmail.com>
2019-04-27 09:01:07 -0400

    Update to analysis dashboard layout

--- analysis-scripts/bib-scripts/original/key-signature.sh
+++ analysis-scripts/bib-scripts/original/key-signature.sh
@@ -1,43 +1,50 @@
 #!/bin/bash
 file=$1
 
-a=$(grep 'k\[' $file)
+b=$(grep 'k\[' $file | tr '\t' '\n')
+
+sigs=$(echo "$b" | wc -l)
 
 suffix="song.txt"
 
 removed=$(echo $file | sed -e "s/$suffix$//")
-output="${removed}data/key-signature/key-signature.txt"
+output="${removed}data/key-signature"
 
-if [ "$a" == "*k[]" ]; then
-    echo C Major > $output
-elif [ "$a" == "*k[f#]" ]; then
-    echo G Major > $output
-elif [ "$a" == "*k[f#c#]" ]; then
-    echo D Major > $output
-elif [ "$a" == "*k[f#c#g#]" ]; then
-    echo A Major > $output
-elif [ "$a" == "*k[f#c#g#d#]" ]; then
-    echo E Major > $output
-elif [ "$a" == "*k[f#c#g#d#a#]" ]; then
-    echo B Major > $output
-elif [ "$a" == "*k[f#c#g#d#a#e#]" ]; then
-    echo F\# Major > $output
-elif [ "$a" == "*k[f#c#g#d#a#e#b#]" ]; then
-    echo C\#  Major > $output
-elif [ "$a" == "*k[b-]" ]; then
-    echo F Major > $output
-elif [ "$a" == "*k[b-e-]" ]; then
-    echo Bb Major > $output
-elif [ "$a" == "*k[b-e-a-]" ]; then
-    echo Eb Major > $output
-elif [ "$a" == "*k[b-e-a-d-]" ]; then
-    echo Ab Major > $output
-elif [ "$a" == "*k[b-e-a-d-g-]" ]; then
-    echo Db Major > $output
-elif [ "$a" == "*k[b-e-a-d-g-c-]" ]; then
-    echo Gb Major > $output
-elif [ "$a" == "*k[b-e-a-d-g-c-f-]" ]; then
-    echo Cb Major > $output
-else
-    echo "No key signature found." > $output
-fi
+for i in $(seq 1 $sigs); do
+    a=$(echo "$b" | sed "${i}q;d")
+    if [ "$a" == "*k[]" ]; then
+        echo C Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[f#]" ]; then
+        echo G Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[f#c#]" ]; then
+        echo D Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[f#c#g#]" ]; then
+        echo A Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[f#c#g#d#]" ]; then
+        echo E Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[f#c#g#d#a#]" ]; then
+        echo B Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[f#c#g#d#a#e#]" ]; then
+        echo F\# Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[f#c#g#d#a#e#b#]" ]; then
+        echo C\#  Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[b-]" ]; then
+        echo F Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[b-e-]" ]; then
+        echo Bb Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[b-e-a-]" ]; then
+        echo Eb Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[b-e-a-d-]" ]; then
+        echo Ab Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[b-e-a-d-g-]" ]; then
+        echo Db Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[b-e-a-d-g-c-]" ]; then
+        echo Gb Major > "${output}/signature-${i}.txt"
+    elif [ "$a" == "*k[b-e-a-d-g-c-f-]" ]; then
+        echo Cb Major > "${output}/signature-${i}.txt"
+    else
+        echo "No key signature found." > "${output}/signature-${i}.txt"
+    fi
+done
+
+
--- analysis.php
+++ analysis.php
@@ -9,44 +9,100 @@
 
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-        <script src="https://verovio-script.humdrum.org/scripts/verovio-toolkit.js"></script>
-        <script src="https://plugin.humdrum.org/scripts/humdrum-notation-plugin.js"></script>
-        <script> var vrvToolkit = new verovio.toolkit(); </script>
 
     </head>
     <body>
 
-<?php
-    $filesrecieved = $_POST['userfilelocations'];
-    $explodedfiledirs = explode(",", $filesrecieved);
+        <div id="navbarr">
+            <ul id=navbar>
+            <span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
 
-    foreach ($explodedfiledirs as $value) {
-        $song = $value . "/song.txt";
-        $datadir = $value . "/data";
-        mkdir($datadir);
-        foreach($_POST['data-choose'] as $selected){
-            $analysisname = substr($selected, 0, strpos($selected, "."));
-            $directory = $value . "/data/" . "$analysisname";
-            mkdir($directory);
-            $scriptdirec = "analysis-scripts/bib-scripts/original/" . $selected;
+                <a href="index.php"><img id="headerbanner" src="image-assets/headerbanner.png" alt="Header logo" height="50px" width="260" align="middle"></a>
+                <span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+                <!-- <li id="Menu" class="logoimage"><img src="headerbanner.png" alt="Header logo" height="60px" width="313px"></img></li> -->
+                <li id="Menu" class="home"><a href="index.php">Home</a></li>
+                <li id="Menu"><a href="pages/about.html">About</a></li>
+                <li id="Menu"><a href="pages/code.html">Code</a></li>
+                <li id="Menu"><a href="pages/research.html">Our Research</a></li>
+                <li id="Menu"><a href="pages/library.html">Database</a></li>
+                <li id="Menu"><a href="pages/analysis-tools.html">Analysis Tools</a></li>
+            </ul>
+        </div>
+        <div id="bumper">
+        </div>
 
-            shell_exec("$scriptdirec $song");
+        <style>
+            .home{
+                background-color: rgb(153, 153, 153);
+                box-shadow: 1px 4px 28px  rgb(0, 0, 0);
+                outline: 0;
+            }
+        </style>
+        <br>
+        <br>
+        <center>
+        <h1>Analysis Dashboard</h1> 
+        </center>
+        <?php
+            $filesrecieved = $_POST['userfilelocations'];
+            $explodedfiledirs = explode(",", $filesrecieved);
+
+            foreach ($explodedfiledirs as $value) {
+                $song = $value . "/song.txt";
+                $datadir = $value . "/data";
+                mkdir($datadir);
+                foreach($_POST['data-choose'] as $selected){
+                    $analysisname = substr($selected, 0, strpos($selected, "."));
+                    $directory = $value . "/data/" . "$analysisname";
+                    mkdir($directory);
+                    $scriptdirec = "analysis-scripts/bib-scripts/original/" . $selected;
+
+                    shell_exec("$scriptdirec $song");
 
 
-            // selected is the script, song is the file
-            //within bash, we can derive the output directory based on the current bash script being run, and the song.txt argument, so no need for an output arg here
-            
-        }
+                    // selected is the script, song is the file
+                    //within bash, we can derive the output directory based on the current bash script being run, and the song.txt argument, so no need for an output arg here
+                    
+                }
 
-    }
-    foreach ($explodedfiledirs as $value) {
-        $datafolder = $value . "/data";
-        $keySigPath = $datafolder . "/key-signature/key-signature.txt";
-        $keysig = file_get_contents($keySigPath);
-        echo '<div class="panels"><p>' . $value . '</p><br><p>' . $keysig . '</p></div>';
-    }
+            }
+
+        $chosentypes = $_POST['data-choose'];
+
+            // for each of the files, echo some divs with data
+            foreach ($explodedfiledirs as $value) {
+
+                $datafolder = $value . "/data";
+
+                // KEY SIGNATURE 
+                // ======================================
+                if (in_array("key-signature.sh", $chosentypes)) {    // if key signature is in the array, echo key sig data
+                    $keySigDir = $datafolder . "/key-signature";
+
+                    $currentKeySigFile = array();
+
+                    foreach (new DirectoryIterator($keySigDir) as $fileInfo) {
+                        if($fileInfo->isDot()) continue;
+                        $currentKeySigFile[] = $fileInfo->getFilename();
+                    }
+
+                    echo "<div class=panels>";
+                    echo "<p>" . $value . "</p>";
+                    foreach ($currentKeySigFile as $readKeySig){
+                        $fullKeySigPath = $keySigDir . "/" . $readKeySig;
+                        echo file_get_contents($fullKeySigPath);
+                        echo "<br>";
+                    }
+                    echo "</div>";
+
+                }
+                // ======================================
 
 
-?>
-</body>
+                    
+            }
+
+
+        ?>
+    </body>
 </html>
