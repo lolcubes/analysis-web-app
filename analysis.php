c35d4a8137ee9f48031201e8518b3ab61170a67f
BeatsInBytes <kaivernooy@gmail.com>
2019-04-30 21:47:57 -0400

    Update to contents

--- analysis.php
+++ analysis.php
@@ -412,94 +412,51 @@
                         // change it based on type!!
                         //=============================
                         if ($analysisname == "total-time") {
+                            $timepath = $value . "/data/total-time/time.txt";
+                            $timecontent = file_get_contents($timepath);
                             echo "
                             <div class=analysis-container_" . $filename . " id=" . $analysisname . "_" . $filename . "  style=\"display: none;\">
                                 <div class=analysis-content>
-                                    <span>" 
-                                        .$analysisname . "
-                                    </span>
-                                    <span>
-                                        oof
+                                    <span>" . 
+                                        $timecontent
+                                    ."
                                     </span>
                                 </div>
                             </div>";
                         
                         }
                         if ($analysisname == "key-signature") {
+                            $datafolder = $value . "/data";
+                            $keySigDir = $datafolder . "/key-signature";
+
+                            $currentKeySigFile = array();
+        
+                            foreach (new DirectoryIterator($keySigDir) as $fileInfo) {
+                                if($fileInfo->isDot()) continue;
+                                $currentKeySigFile[] = $fileInfo->getFilename();
+                            }
+        
+                            $keySigsRead = array();
+
+                            foreach ($currentKeySigFile as $readKeySig){
+                                $fullKeySigPath = $keySigDir . "/" . $readKeySig;
+                                $keySigsRead[] = file_get_contents($fullKeySigPath);
+                            }
+
+
                             echo "
                             <div class=analysis-container_" . $filename . " id=" . $analysisname . "_" . $filename . "  style=\"display: none;\">
                                 <div class=analysis-content>
-                                    <span>"
-                                        . $analysisname . "
-                                    </span>
                                     <span>
-                                        <div id=container" . $value . "></div>
-                                        <script>
-                                            Highcharts.chart('container" . $value . "', {
-
-                                                title: {
-                                                text: 'Solar Employment Growth by Sector, 2010-2016'
-                                                },
-                                            
-                                                subtitle: {
-                                                text: 'Source: thesolarfoundation.com'
-                                                },
-                                            
-                                                yAxis: {
-                                                title: {
-                                                    text: 'Number of Employees'
-                                                }
-                                                },
-                                                legend: {
-                                                layout: 'vertical',
-                                                align: 'right',
-                                                verticalAlign: 'middle'
-                                                },
-                                            
-                                                plotOptions: {
-                                                series: {
-                                                    label: {
-                                                    connectorAllowed: false
-                                                    },
-                                                    pointStart: 2010
-                                                }
-                                                },
-                                            
-                                                series: [{
-                                                name: 'Installation',
-                                                data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-                                                }, {
-                                                name: 'Manufacturing',
-                                                data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-                                                }, {
-                                                name: 'Sales & Distribution',
-                                                data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-                                                }, {
-                                                name: 'Project Development',
-                                                data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-                                                }, {
-                                                name: 'Other',
-                                                data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-                                                }],
-                                            
-                                                responsive: {
-                                                rules: [{
-                                                    condition: {
-                                                    maxWidth: 500
-                                                    },
-                                                    chartOptions: {
-                                                    legend: {
-                                                        layout: 'horizontal',
-                                                        align: 'center',
-                                                        verticalAlign: 'bottom'
-                                                    }
-                                                    }
-                                                }]
-                                                }
-                                            
-                                            });
-                                          </script>
-                                        KEYOBOI
+                                        Key Signatures:
+                                    </span>
+                                    <br>
+                                    <span>";
+                                    foreach ($keySigsRead as $currentKeySigRead) {
+                                        echo $currentKeySigRead;
+                                        echo "<br>";
+                                    }
+                                    echo "
                                     </span>
                                 </div>
                              </div>";
