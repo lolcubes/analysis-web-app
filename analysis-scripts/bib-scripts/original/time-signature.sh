7f0874b833cd0c0dac86a843b661f9d4e31e75a2
BeatsInBytes <kaivernooy@gmail.com>
2019-05-09 16:16:38 -0400

    Added percents to time-sig

--- analysis-scripts/bib-scripts/original/time-signature.sh
+++ analysis-scripts/bib-scripts/original/time-signature.sh
@@ -7,12 +7,38 @@ removed=$(echo $file | sed -e "s/$suffix$//")
 output="${removed}data/time-signature"
 
 
-a=$(grep '\*M' $file | grep -v 'MM' | tr '\t' '\n' | while read line; do echo $line | cut -c 3-; done)
+sigs=$(grep '\*M' $file | grep -v 'MM' | tr '\t' '\n' | while read line; do echo $line | cut -c 3-; done)
 
-number=$(echo "$a" | wc -l)
+number=$(echo "$sigs" | wc -l)
 
 for i in $(seq 1 $number); do
-    echo "$a" | sed "${i}q;d" > "${output}/signature-${i}.txt"
+    echo "$sigs" | sed "${i}q;d" > "${output}/signature-${i}.txt"
+    echo "$sigs" | sed "${i}q;d" >> "${output}/occurrences.txt"
 done
 
-#if $a=
+uniq=$(cat "${output}/occurrences.txt" | sort | uniq -c | sed -e 's/^[ \t]*//' | tr ' ' '~')
+lines=$(cat "${output}/occurrences.txt" | wc -l )
+numbers=$(echo "$uniq" | cut -d '~' -f1)
+vals=$(echo "$uniq" | cut -d '~' -f2)
+percents=$(echo "$numbers" | while read line; do echo "$line/$lines" | bc -l; done )
+seqVal=$(echo "$percents" | wc -l)
+
+seqValMinus=$(echo "$seqVal - 1" | bc -l)
+
+if [ $seqValMinus -lt "1" ]; then
+
+    echo "$percents" | tr -d '\n' >> "${output}/occurrences-percents.txt"
+    echo "$vals" | tr -d '\n' >> "${output}/occurrences-values.txt"
+
+elif [ $seqValMinus -ge "1" ]; then
+
+    for x in $(seq 1 $seqValMinus); do
+        echo "$percents" | sed "${x}q;d" | tr -d '\n' | sed 's/$/,/' | tr -d '\n' >> "${output}/occurrences-percents.txt"
+    done
+    echo "$percents" | sed "${seqVal}q;d" | tr -d '\n' >> "${output}/occurrences-percents.txt"
+    for x in $(seq 1 $seqValMinus); do
+        echo "$vals" | sed "${x}q;d" | tr -d '\n' | sed 's/$/,/' | tr -d '\n' >> "${output}/occurrences-values.txt"
+    done
+    echo "$vals" | sed "${seqVal}q;d" | tr -d '\n' >> "${output}/occurrences-values.txt"
+
+fi
