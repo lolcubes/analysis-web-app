a5e5a7b826ecf09abe363f417756785f72d0e956
BeatsInBytes <kaivernooy@gmail.com>
2019-05-18 14:19:44 -0400

    Removed non-integers

--- analysis-scripts/bib-scripts/original/average-pitch.sh
+++ analysis-scripts/bib-scripts/original/average-pitch.sh
@@ -8,11 +8,11 @@ output="${removed}data/average-pitch/pitch.txt"
 movingOutput="${removed}data/average-pitch/pitch-moving-average.txt"
 
 # Prepares the file by turning it into a single column list of numbers to be processed
-filePrep=$(/Applications/MAMP/htdocs/NewTestings/analysis-scripts/humdrum/deg/deg $file | grep -v '=' | grep -v '*' | grep -v '!' | grep -v 'r' | tr -d '^' | tr -d 'v' | tr -d . | tr '\t' '\n' | tr ' ' '\n' | grep .);
+filePrep=$(/Applications/MAMP/htdocs/NewTestings/analysis-scripts/humdrum/deg/deg $file | grep -v '=' | grep -v '*' | grep -v '!' | grep -v 'r' | tr -d '^' | tr -d 'v' | tr -d . | tr '\t' '\n' | tr ' ' '\n' | grep . );
 
 # Converts the scale degree into numbers
-tradeDecim=$( echo "$filePrep" | sed '/^$/d' | sed 's/+/.5/g' | sed 's/1-/0.5/g' | sed 's/2-/1.5/g' | sed 's/3-/2.5/g' | sed 's/4-/3.5/g' | sed 's/5-/4.5/g' | sed 's/6-/5.5/g' | sed 's/7-/6.5/g' | grep -v 'p' | grep -v 'f' | grep -v 'm' | grep -v '<' | grep -v '>');
-
+tradeDecim=$( echo "$filePrep" | sed '/^$/d' | sed 's/+/.5/g' | sed 's/1-/0.5/g' | sed 's/2-/1.5/g' | sed 's/3-/2.5/g' | sed 's/4-/3.5/g' | sed 's/5-/4.5/g' | sed 's/6-/5.5/g' | sed 's/7-/6.5/g' | grep -v '>' | grep '[0-9]' | grep -v '[a-z]' | grep -v '[A-Z]');
+echo "$tradeDecim"
 # Sums the converted numbers
 sumDecim=$(echo "$tradeDecim" | tr '\n' '+' | awk '{print $0"0"}' | bc -l);
 
