c87cc854ec0e836eb0baa498afb99d56ef93d899
BeatsInBytes <kaivernooy@gmail.com>
2019-05-18 13:58:43 -0400

    Delete dynam marks

--- analysis-scripts/bib-scripts/original/average-pitch.sh
+++ analysis-scripts/bib-scripts/original/average-pitch.sh
@@ -11,7 +11,7 @@ movingOutput="${removed}data/average-pitch/pitch-moving-average.txt"
 filePrep=$(/Applications/MAMP/htdocs/NewTestings/analysis-scripts/humdrum/deg/deg $file | grep -v '=' | grep -v '*' | grep -v '!' | grep -v 'r' | tr -d '^' | tr -d 'v' | tr -d . | tr '\t' '\n' | tr ' ' '\n' | grep .);
 
 # Converts the scale degree into numbers
-tradeDecim=$( echo "$filePrep" | sed '/^$/d' | sed 's/+/.5/g' | sed 's/1-/0.5/g' | sed 's/2-/1.5/g' | sed 's/3-/2.5/g' | sed 's/4-/3.5/g' | sed 's/5-/4.5/g' | sed 's/6-/5.5/g' | sed 's/7-/6.5/g');
+tradeDecim=$( echo "$filePrep" | sed '/^$/d' | sed 's/+/.5/g' | sed 's/1-/0.5/g' | sed 's/2-/1.5/g' | sed 's/3-/2.5/g' | sed 's/4-/3.5/g' | sed 's/5-/4.5/g' | sed 's/6-/5.5/g' | sed 's/7-/6.5/g' | grep -v 'p' | grep -v 'f' | grep -v 'm' | grep -v '<' | grep -v '>');
 
 # Sums the converted numbers
 sumDecim=$(echo "$tradeDecim" | tr '\n' '+' | awk '{print $0"0"}' | bc -l);
@@ -26,4 +26,3 @@ bash /Applications/MAMP/htdocs/NewTestings/analysis-scripts/other/moving-average
 # Divides the sum of the pitches by the number of notes for an average
 
 bc -l <<< $sumDecim/$totalLines > $output
-
