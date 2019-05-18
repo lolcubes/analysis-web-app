0c934c894c04c05ccf9f169c76e438801ff01d1d
BeatsInBytes <kaivernooy@gmail.com>
2019-05-17 20:25:00 -0400

    Added conditionals

--- formula_single.sh
+++ formula_single.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
-echo "test"
+
+songfile=$1
+comparefile=$2
+
+suffix="song.txt"
+songdir=$(echo $songfile | sed -e "s/$suffix$//")
+datatypes="${songdir}dataTypes.txt"
+datatypes=$(cat $datatypes)
+
+echo "$datatypes" | while read line; do
+
+    if [ "$line" == "scales" ]; then echo scales
+
+        elif [ "$line" == "key-signature" ]; then echo key-signature
+
+        elif [ "$line" == "average-pitch" ]; then echo average-pitch
+
+        elif [ "$line" == "average-steps" ]; then echo average-steps
+
+        elif [ "$line" == "most-used-pitches" ]; then echo most-used-pitches
+
+        elif [ "$line" == "average-note-value" ]; then echo average-note-value
+
+        elif [ "$line" == "repeated-note-value" ]; then echo repeated-note-value
+
+        elif [ "$line" == "repeated-pitches" ]; then echo repeated-pitches
+
+        elif [ "$line" == "time-signature" ]; then echo time-signature
+
+        elif [ "$line" == "most-used-note-value" ]; then echo most-used-note-value
+
+    fi
+
+done
