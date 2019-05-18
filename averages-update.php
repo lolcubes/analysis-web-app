61b53853485d3b58fd210ba4ce41d7a8936dc8ae
BeatsInBytes <kaivernooy@gmail.com>
2019-05-18 16:40:00 -0400

    Cleaned gitignore

--- averages-update.php
+++ averages-update.php
@@ -1,12 +1,17 @@
 <?php
-$files = scandir('Song_Database/');
-foreach($files as $file) {
-    $filetypes = 'Song_Database/' . $file . "/dataTypes.txt";
-    // echo $filetypes;
-    echo file_get_contents($filetypes);
-    echo "<br>";
-    
-}
+    $files = scandir('Song_Database/');
+    foreach($files as $file) {
+        $filetypes = 'Song_Database/' . $file . "/dataTypes.txt";
+        $composer = 'Song_Database/' . $file . "/time-info/composer.txt";
+        $period = 'Song_Database/' . $file . "/time-info/period.txt";
 
+        // echo $filetypes;
+        // echo nl2br(file_get_contents($filetypes));
 
+        echo file_get_contents($composer);
+        echo file_get_contents($period);
+
+        echo "<br>";
+        
+    }
 ?>
