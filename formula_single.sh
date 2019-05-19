4baedce7db8320caa4cc441c34b260e715c29e62
BeatsInBytes <kaivernooy@gmail.com>
2019-05-19 17:19:48 -0400

    Removed p-error file formula

--- formula_single.sh
+++ formula_single.sh
@@ -3,34 +3,17 @@
 songfile=$1
 comparefile=$2
 
-suffix="song.txt"
-songdir=$(echo $songfile | sed -e "s/$suffix$//")
-datatypes="${songdir}dataTypes.txt"
-datatypes=$(cat $datatypes)
 
-echo "$datatypes" | while read line; do
+function percentError() {
+    val1=$1
+    val2=$2
 
-    if [ "$line" == "scales" ]; then echo scales
-
-        elif [ "$line" == "key-signature" ]; then echo key-signature
-            cat "${songdir}data/key-signature/occurrences-values.txt"
-            printf "\n"
-        elif [ "$line" == "average-pitch" ]; then echo average-pitch
-
-        elif [ "$line" == "average-steps" ]; then echo average-steps
-
-        elif [ "$line" == "most-used-pitches" ]; then echo most-used-pitches
-
-        elif [ "$line" == "average-note-value" ]; then echo average-note-value
-
-        elif [ "$line" == "repeated-note-value" ]; then echo repeated-note-value
-
-        elif [ "$line" == "repeated-pitches" ]; then echo repeated-pitches
-
-        elif [ "$line" == "time-signature" ]; then echo time-signature
-
-        elif [ "$line" == "most-used-note-value" ]; then echo most-used-note-value
-
+    if (( $(echo "$val1 > $val2" | bc -l) )); then
+        echo "scale=5;1-((${val1}-${val2})/${val1})" | bc -l
+    else
+        echo "scale=5;1-((${val2}-${val1})/${val2})" | bc -l
     fi
 
-done
+}
+
+percentError $1 $2
