4a2c4791429f98f5dbaac315a06a632be1e24879
BeatsInBytes <kaivernooy@gmail.com>
2019-06-03 16:49:41 -0400

    Delete test

--- pages/contact.php
+++ pages/contact.php
@@ -1,29 +1,14 @@
 <?php 
-    // $name = $_POST['name'];
-    // $email = $_POST['email'];
-    // $subject = $_POST['subject'];
-    // $message = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-    // $subjectLine = "BiB | " . $subject;
-    // $from = "From: " . $email;
+    $subjectLine = "BiB | " . $subject;
+    $from = "From: " . $email;
 
-    // // mail("kaivernooy@gmail.com", "$subjectLine", $message, $from);
+    mail("kaivernooy@gmail.com", "$subjectLine", $message, $from);
 
-    $sender = 'beatsinbytesofficial@gmail.com';
-    $recipient = 'kaivernooy@gmail.com';
-    
-    $subject = "php mail test";
-    $message = "php test message";
-    $headers = 'From:' . $sender;
-    
-    if (mail($recipient, $subject, $message, $headers))
-    {
-        echo "Message accepted";
-    }
-    else
-    {
-        echo "Error: Message not accepted";
-    }
 
 
 ?>
