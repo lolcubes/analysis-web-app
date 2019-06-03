9e512ee1e3104930e6ab06142670dd95c53872a8
BeatsInBytes <kaivernooy@gmail.com>
2019-06-03 16:42:07 -0400

    Mail function test

--- pages/contact.php
+++ pages/contact.php
@@ -1,19 +1,29 @@
 <?php 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    // $name = $_POST['name'];
+    // $email = $_POST['email'];
+    // $subject = $_POST['subject'];
+    // $message = $_POST['message'];
 
-    $subjectLine = "BiB | " . $subject;
-    $from = "From: " . $email;
+    // $subjectLine = "BiB | " . $subject;
+    // $from = "From: " . $email;
 
-    // mail("kaivernooy@gmail.com", "$subjectLine", $message, $from);
+    // // mail("kaivernooy@gmail.com", "$subjectLine", $message, $from);
 
-    $messago = "Line 1\r\nLine 2\r\nLine 3";
-
-    // In case any of our lines are larger than 70 characters, we should use wordwrap()
-    $messago = wordwrap($messago, 70, "\r\n");
+    $sender = 'beatsinbytesofficial@gmail.com';
+    $recipient = 'kaivernooy@gmail.com';
     
-    // Send
-    mail('kaivernooy@gmail.com', 'My Subject', $messago);
+    $subject = "php mail test";
+    $message = "php test message";
+    $headers = 'From:' . $sender;
+    
+    if (mail($recipient, $subject, $message, $headers))
+    {
+        echo "Message accepted";
+    }
+    else
+    {
+        echo "Error: Message not accepted";
+    }
+
+
 ?>
