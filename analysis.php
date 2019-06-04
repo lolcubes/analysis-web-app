1297a04f76bfd76f94df6afbdf2197753d250686
BeatsInBytes <kaivernooy@gmail.com>
2019-06-04 17:44:44 -0400

    Update to sparkline graphs

--- analysis.php
+++ analysis.php
@@ -1423,10 +1423,10 @@
                             echo "
                             <div class=analysis-container_" . $filename . " id=general" . "_" . $filename .  " style='width: 100%; transition: all .4s ease;'>
                             <div class=analysis-content>
-
-                                <div id=pitchChart" . $filename . " class=chart-container style='margin-left:0;margin-right:4%;display:inline-block;width:40%;padding:10px;height:30%'></div>
-                                <div id=pieChart" . $filename . " class=chart-container style='display:inline-block;width:40%;padding:10px;height:30%'></div>
-
+                                <div class=sparklines style='width:45%;'>
+                                    <div id=pitchChart" . $filename . " class=chart-container style='margin-left:0;margin-right:4%;width:40%;padding:10px;height:30%'></div>
+                                    <div id=pieChart" . $filename . " class=chart-container style='width:40%;padding:10px;height:30%'></div>
+                                </div>
                                 <script>
                                     var options = {
                                     chart: {
