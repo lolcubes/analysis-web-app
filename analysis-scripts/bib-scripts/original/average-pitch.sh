ba1951e19473790b2459191ab3f86926c4a7e586
BeatsInBytes <kaivernooy@gmail.com>
2019-06-03 23:59:30 -0400

    Debug the empty list

--- analysis-scripts/bib-scripts/original/average-pitch.sh
+++ analysis-scripts/bib-scripts/original/average-pitch.sh
@@ -14,6 +14,8 @@ filePrep=$(cat $deg | grep -v '=' | grep -v '*' | grep -v '!' | grep -v 'r' | tr
 # Converts the scale degree into numbers
 tradeDecim=$( echo "$filePrep" | sed '/^$/d' | sed 's/+/.5/g' | sed 's/1-/0.5/g' | sed 's/2-/1.5/g' | sed 's/3-/2.5/g' | sed 's/4-/3.5/g' | sed 's/5-/4.5/g' | sed 's/6-/5.5/g' | sed 's/7-/6.5/g' | grep -v '>' | grep '[0-9]' | grep -v '[a-z]' | grep -v '[A-Z]' | sed 's/[^0-9]*//g' | grep .);
 
+echo "$tradeDecim" > $movingOutput
+
 # Sums the converted numbers
 sumDecim=$(echo "$tradeDecim" | tr '\n' '+' | awk '{print $0"0"}' | bc -l);
 
@@ -22,7 +24,7 @@ totalLines="$(echo "$filePrep" | wc -l)";
 
 lineCountHalf=$(echo "scale=0;$totalLines/2" | bc -l)
 
-bash /var/www/html/analysis-scripts/other/moving-average.sh "$tradeDecim" $lineCountHalf | paste -sd, - | tr -d '\n' > $movingOutput
+bash /var/www/html/analysis-scripts/other/moving-average.sh "$( cat $movingOutput)" $lineCountHalf | paste -sd, - | tr -d '\n' > $movingOutput
 
 # Divides the sum of the pitches by the number of notes for an average
 
