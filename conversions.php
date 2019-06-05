a345306ecb4fc7c9147be30028ebc901e3f86afe
BeatsInBytes <kaivernooy@gmail.com>
2019-06-05 16:04:58 -0400

    Update to arrays

--- conversions.php
+++ conversions.php
@@ -1,6 +1,21 @@
 <?php
 
-$test = $_POST['downloadsFilesTypes'];
-echo $test;
+$recieved = $_POST['downloadsFilesTypes'];
+$recieved = explode(",", $recieved);
+
+
+$files = array();
+$types = array();
+
+foreach ($recieved as $value) {
+    $testFile = substr($value, 0, 17);
+    if ($testFile == "dropzonefileicons"){
+        $files[] = $value
+    }
+    else {
+        $types[] = $value
+    }
+}
+print_r($files);
 
 ?>
